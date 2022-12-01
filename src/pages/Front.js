eb90ea64083ab18188f0783b646ad1368046573d
Henri Kontio <henri.kontio@solita.fi>
2022-12-01 09:48:53 +0200

    Add some info of workshop

--- src/pages/Front.js
+++ src/pages/Front.js
@@ -5,6 +5,17 @@ const Front = () => {
     <div className={'main-content'}>
       <h3>Welcome to a11y workshop</h3>
       
+      <p>Some things we're going through today:</p>
+      <p>
+        - Basics<br />
+        - Tooling<br />
+        - Images<br />
+        - Headings<br />
+        - Basic forms<br />
+        - Contrasts, text<br />
+        - WAI-ARIA
+      </p>
+
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi in leo tellus. Quisque enim nisi, placerat at
         nisi ut, varius euismod lectus. Cras quis erat et nibh bibendum pharetra in eu magna. Maecenas pulvinar
         malesuada quam, ut iaculis metus molestie eget. Nullam vel lorem non magna congue interdum ut eu diam. Cras
@@ -12,6 +23,8 @@ const Front = () => {
       
       <img src={'https://picsum.photos/700/300?random=1'} />
       <span className={'img-caption'}>This image is really cool.</span>
+
+      <h5>The next section is super interesting for you.</h5>
 
       <p>Fusce suscipit lacus turpis, ut posuere elit tempor sit amet. Phasellus cursus porttitor scelerisque. Fusce
         pharetra dui a porttitor ultrices. Curabitur ac sapien ante. Quisque varius nec lectus quis ultricies. Donec
